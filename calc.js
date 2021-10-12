698ebce33187c175aeccfd3e16b92cc983c361b0
ruthfee zota <ruthfeezota@gmail.com>
2021-10-12 15:18:22 -0400

    Addition implementation working

--- calc.js
+++ calc.js
@@ -15,7 +15,21 @@ reader.question("What would you like to calculate?", function(input){
 	num2 = Number(tokens[2]);
 
 
+	if (mathSymbol === "+"){
+			console.log(num1 + num2);
+	} else if (mathSymbol === "-"){
+			console.log(num1 - num2);
+	} else if (mathSymbol === "*"){	
+			console.log(num1 * num2); 
+	} else if (mathSymbol ==="/"){
+			console.log(num1 / num2);
+	} else  (mathSymbol === "square"){
+			console.log(Math.sqrt (num1, num2));
+	}
+	
+
 	// This line closes the connection to the command line interface.
 	reader.close()
 
 });
+``
